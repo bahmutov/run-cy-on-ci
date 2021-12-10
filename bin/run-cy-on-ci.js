81aae74862f2e0741cc71e48a87bd10584a196e2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-10 15:48:58 -0500

    bring in as-a

--- bin/run-cy-on-ci.js
+++ bin/run-cy-on-ci.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
 
 const debug = require('debug')('run-cy-on-ci')
+
+const arg = require('arg')
+const args = arg({
+  '--grep': String,
+
+  // aliases
+  '-g': '--grep',
+})
+debug('arguments %o', args)
+
+if (!args['--grep'] && args._.length === 1) {
+  debug('user entered just the grep after the command')
+  args['--grep'] = args._[0]
+}
+
+if (!args['--grep']) {
+  console.error('Need part of the title to grep for')
+  console.error('--grep <part of title>')
+  process.exit(1)
+}
+
+const { getSettings } = require('as-a')
+const settings = getSettings('.')
+console.log(settings)
