6ee68816f3b89f610e733596b2175d19fa3cb133
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-13 17:15:07 -0500

    add prettier

--- bin/run-cy-on-ci.js
+++ bin/run-cy-on-ci.js
@@ -6,11 +6,14 @@ const triggerCircle = require('trigger-circleci-pipeline')
 const arg = require('arg')
 const args = arg({
   '--grep': String,
+  '--tags': String,
   '--burn': Number,
 
   // aliases
   '-g': '--grep',
-  '-b': '--burn'
+  '-b': '--burn',
+  '-t': '--tags',
+  '--tag': '--tags',
 })
 debug('arguments %o', args)
 
@@ -19,25 +22,36 @@ if (!args['--grep'] && args._.length === 1) {
   args['--grep'] = args._[0]
 }
 
-if (!args['--grep']) {
+if (!args['--grep'] && !args['--tags']) {
   console.error('Need part of the title to grep for')
   console.error('--grep <part of title>')
+  console.error('or some tags with --tag <tag>')
   process.exit(1)
 }
 
 const { getSettings } = require('as-a')
 const settings = getSettings('run-cy-on-ci')
-debug('got settings for "run-cy-on-ci" with the following keys %o', Object.keys(settings))
+debug(
+  'got settings for "run-cy-on-ci" with the following keys %o',
+  Object.keys(settings),
+)
 
-triggerCircle.triggerPipeline({
-  org: settings.CIRCLE_CI_ORG,
-  project: settings.CIRCLE_CI_PROJECT,
-  parameters: {
-    GREP: args['--grep'],
-    BURN: args['--burn'] || 1,
-  },
-  circleApiToken: settings.CIRCLE_CI_API_TOKEN
-}).catch(err => {
-  console.error(err)
-  process.exit(1)
-})
+const org = settings.CIRCLE_CI_ORG
+const project = settings.CIRCLE_CI_PROJECT
+const parameters = {
+  GREP: args['--grep'],
+  GREP_TAGS: args['--tags'],
+  BURN: args['--burn'] || 1,
+}
+
+triggerCircle
+  .triggerPipeline({
+    org,
+    project,
+    parameters,
+    circleApiToken: settings.CIRCLE_CI_API_TOKEN,
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
